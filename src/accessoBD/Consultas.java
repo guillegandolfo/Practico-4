2f32c2d402dcea08979718aababd0e15eaa15377
guillegandolfo <guillegandolfo@hotmail.com>
2018-10-17 00:10:47 -0300

    va balaaa

--- src/accessoBD/Consultas.java
+++ src/accessoBD/Consultas.java
@@ -13,7 +13,6 @@ public class Consultas {
 		return "select max(numero) from Juridico.Revisiones where codigoFolio = (?)";
 	}
 	
-	//Revisiones (número INT, codigoFolio VARCHAR(60), descripcion VARCHAR(60)) 
 	public String InsertarRevision(){
 		return "insert into Juridico.Revisiones Value('?','?','?')";
 	}
@@ -23,5 +22,24 @@ public class Consultas {
 		return "INSERT INTO Juridico.Folios VALUES (?,?,?)";
 	}
 	
+	public String eliminarFolio(){
+		return "delete from Juridico.Folios where codigo = (?)";
+	}
+	
+	public String eliminarRevisiones(){
+		return "delete from Juridico.Revisiones where codigoFolio = (?)";
+	}
+	
+	public String getDescripcion(){
+		return "select descripcion from Juridico.Revisiones where codigoFolio = (?) and numero = (?)";
+	}
 
+	public String listarFolios(){
+		return "select * from Juridico.Folios order by codigo";
+	}
+	
+	public String listarRevisiones(){
+		return "select * from Juridico.Revisiones where codigoFolio = (?) order by numero";
+	}
+	
 }
--- src/persistencia/Fachada.java
+++ src/persistencia/Fachada.java
@@ -10,7 +10,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 
+import VO.VORevision;
 import VO.VoFolio;
 import Excepciones.Exc_Persistencia;
 import accessoBD.Consultas;
@@ -105,18 +108,15 @@ public class Fachada extends UnicastRemoteObject {
 		ResultSet rs = pstmt.executeQuery();
 		//Consulto si existe 
 		if (rs.next()){
-			//no hago nada por que existe el Folio
-		}else
-		{
 			int maxrevisionid;
 			query = consulta.MaxFolioId();
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, CodFolio);
 			rs = pstmt.executeQuery();
+			//Si tiene revisiones, obtengo la mayor y si no es 0
 			if (rs.next()){
 				 maxrevisionid = rs.getInt(1);
-			}else
-			{
+			}else{
 				maxrevisionid = 0;
 			}
 			maxrevisionid ++;
@@ -126,21 +126,110 @@ public class Fachada extends UnicastRemoteObject {
 			pstmt.setString(2, CodFolio);
 			pstmt.setString(3, Desc);
 			pstmt.executeUpdate();
-			rs.close();
-			pstmt.close();
-			con.close();
-			
-		}	
-			}
-
-	public void borrarFolioRevisiones(){
+		}
 		
+		rs.close();
+		pstmt.close();
+		con.close();	
+			
+	}
+
+	public void borrarFolioRevisiones(String codigo) throws SQLException{
+		
+		Connection con = DriverManager.getConnection(url, usuario, password);
+		Consultas consulta = new Consultas();
+		String query = consulta.existeFolios();
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setString(1, codigo);
+		ResultSet rs = pstmt.executeQuery();
+		//Consulto si existe 
+		if (rs.next()){
+			//Elimino las revisiones
+			query = consulta.eliminarRevisiones();
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, codigo);
+			pstmt.executeUpdate();
+			
+			//Elimino el Folio
+			query = consulta.eliminarFolio();
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, codigo);
+			pstmt.executeUpdate();
+		}
+		rs.close();
+		pstmt.close();
+		con.close();
 	}
 	
-	public String darDescripcion(){
-		return "";
+	public String darDescripcion(String codigo, int numero) throws SQLException{
+		
+		Connection con = DriverManager.getConnection(url, usuario, password);
+		Consultas consulta = new Consultas();
+		String query = consulta.getDescripcion();
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setString(1, codigo);
+		pstmt.setInt(2, numero);
+		ResultSet rs = pstmt.executeQuery();
+		String Resultado = "";
+		if (rs.next()){
+			Resultado = rs.getString(1);
+		}
+		
+		return Resultado;
 	}
 
+	public LinkedList <VoFolio> listarFolios() throws SQLException{
+		
+		Connection con = DriverManager.getConnection(url, usuario, password);
+		LinkedList <VoFolio> Lista = new LinkedList <VoFolio>();
+		Statement stmt;
+		stmt = con.createStatement();
+		Consultas consulta = new Consultas();
+		String query = consulta.listarFolios();
+		ResultSet rs = stmt.executeQuery(query);
+		
+		//Recorro resultados
+		while (rs.next())
+		{
+			String Codigo = rs.getString("codigo");
+			String Caratula = rs.getString("caratula");
+			int Paginas = rs.getInt("paginas");
+			
+			VoFolio Folio = new VoFolio(Codigo, Caratula, Paginas);
+			Lista.add(Folio);
+		}
+		return Lista;
+	}
 
+	public LinkedList <VORevision> listarRevisiones(String Codigo) throws SQLException{
+		
+		Connection con = DriverManager.getConnection(url, usuario, password);
+		LinkedList <VORevision> Lista = new LinkedList <VORevision>();
+		Consultas consulta = new Consultas();
+		String query = consulta.existeFolios();
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setString(1, Codigo);
+		ResultSet rs = pstmt.executeQuery();
+		//Consulto si existe Folio
+		if (rs.next()){
+			
+			query = consulta.listarRevisiones();		
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, Codigo);
+			rs = pstmt.executeQuery();		
+			
+			//Recorro resultados
+			while (rs.next()){
+				int Numero = rs.getInt("numero");
+				String CodigoFolio = rs.getString("codigoFolio");
+				String Descripcion = rs.getString("descripcion");
+				
+				VORevision Revision = new VORevision(Numero, CodigoFolio, Descripcion);
+				Lista.add(Revision);
+			}
+		}
+
+		return Lista;
+	}
 }
 
